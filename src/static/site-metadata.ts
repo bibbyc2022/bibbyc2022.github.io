dbd92992f19ddfa16a2a73f64b894448aeedb9bf
bibbyc2022 <118505436+bibbyc2022@users.noreply.github.com>
2025-12-22 19:24:31 +0800

    Update site URL to use HTTPS

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'http://running.cc.cd',
+  siteUrl: 'https://running.cc.cd',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
